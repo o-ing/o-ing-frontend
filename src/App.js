b6669fd8aa70c78825f95d1bf48eee4ccd4f4885
jinn2u <bsybear623@gmail.com>
2021-09-27 10:38:23 +0900

    feat: [Header] 디자인

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import "antd/dist/antd.css";
 import Login from "./auth/container/Login";
 import Club from "./club/container/Club";
 import CreatePost from "./createPost/container/CreatePost";
+import Header from "./header/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Route exact path="/" component={Club} />
       <Route path="/signup" component={Signup} />
       <Route path="/login" component={Login} />
---
+++ src/header/Header.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import header_oing from "../asset/header/header_oing.png";
+import useIsLogIn from "../auth/hooks/useIsLogIn";
+import { Link } from "react-router-dom";
+import Login from "../auth/container/Login";
+
+export default function Header() {
+  const isLogIn = useIsLogIn();
+  return (
+    <HeaderStyle>
+      <StyledNav>
+        <Link to="/">
+          <img src={header_oing} alt="oing header img" />
+        </Link>
+        {!isLogIn && <Link to="/">로그아웃</Link>}
+        {isLogIn && (
+          <>
+            <Link to="/login">로그인</Link>
+            <Link to="/signup">회원가입</Link>
+          </>
+        )}
+      </StyledNav>
+    </HeaderStyle>
+  );
+}
+
+const HeaderStyle = styled.div`
+  background-color: white;
+  height: 80px;
+  border-bottom: 1px solid #f2f4ff;
+  padding-top: calc(80px / 4);
+`;
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-left: 64px;
+  padding-right: 64px;
+
+  img {
+    width: 44px;
+  }
+  a:first-child {
+    margin-right: auto;
+  }
+
+  border: 1px solid red;
+  max-width: 1200px;
+  margin: auto auto;
+
+  a:not(:first-child) {
+    color: rgb(254, 185, 145);
+    margin-left: 10px;
+  }
+  @media (max-width: 600px) {
+    padding-left: 34px;
+    padding-right: 34px;
+  }
+`;
